515d3acdc81e21bfd108a1a8bdd2d645fb32c7f7
Nicholas Frank <nick.herko.frank@gmail.com>
2023-11-29 20:35:39 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import express from "express";
+import session from "express-session";
+import cors from "cors";
+
+import UserRoutes from "./users/routes.js";
+import LikesRoutes from "./likes/routes.js";
+import FollowsRoutes from "./follows/routes.js";
+
+mongoose.connect("mongodb://127.0.0.1:27017/project");
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+app.use(session(sessionOptions));
+
+app.use(express.json());
+
+FollowsRoutes(app);
+LikesRoutes(app);
+UserRoutes(app);
+
+app.listen(4000);
