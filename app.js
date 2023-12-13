9112c4f794c7e3284e1eab43b62a0f55c64cea56
Nicholas Frank <nick.herko.frank@gmail.com>
2023-12-13 12:30:09 -0500

    Fix missing import

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import session from "express-session";
 import cors from "cors";
+import "dotenv/config";
 
 import UserRoutes from "./users/routes.js";
 import LikesRoutes from "./likes/routes.js";
